400c5466030b31fa04d6f180da1c93ee2ca62096
Asghar Ali <syed.aaaasghar123@gmail.com>
2021-12-15 11:43:34 +0500

    new

--- shop/urls.py
+++ shop/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="ShopHome"),
-    path("about/", views.about, name="AboutUs"),
-    path("contact/", views.contact, name="ContactUs"),
-    path("tracker/", views.tracker, name="TrackingStatus"),
-    path("search/", views.search, name="Search"),
-    path("products/<int:myid>", views.productView, name="ProductView"),
-    path("checkout/", views.checkout, name="Checkout"),
+    path("shop/about/", views.about, name="AboutUs"),
+    path("shop/contact/", views.contact, name="ContactUs"),
+    path("shop/tracker/", views.tracker, name="TrackingStatus"),
+    path("shop/search/", views.search, name="Search"),
+    path("shop/products/<int:myid>", views.productView, name="ProductView"),
+    path("shop/checkout/", views.checkout, name="Checkout"),
 ]
